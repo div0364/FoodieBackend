4ee8a915fc1f4a59374154fb6f5d1c35f75fd4bd
div0364 <divyam.d.sinha@gmail.com>
2024-08-02 14:29:27 +0530

    third2

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
     next();
 });
 
